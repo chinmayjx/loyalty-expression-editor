dca96f9be17de0438d32cc4aaed0a7f53f80a496
chinmayji <chinmayjain08@gmail.com>
2022-06-21 18:19:53 +0530

    basic setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 const images = {
   cross: require("./images/cross.svg").default,
+  trash: require("./images/trash.png"),
 };
 
 export default function App() {
@@ -103,10 +104,91 @@ function ConditionsBlock({
   );
 }
 
+const anchoredStyle = {
+  fontSize: "0.9rem",
+  position: "absolute",
+  top: "0",
+  transform: "translate(0, calc(-50% - 0.1rem))",
+  backgroundColor: "white",
+  padding: "0 0.4rem",
+};
+
 function ConditionEditBlock() {
   return (
     <div className="main-flex-item">
       <div className="block-title-box">Condition 1</div>
+      <div style={{ padding: "0.5rem 1rem" }}>
+        <div
+          style={{
+            fontWeight: "var(--w6)",
+            marginBottom: "1rem",
+            fontSize: "0.93rem",
+          }}
+        >
+          Expression
+        </div>
+        <div
+          style={{
+            borderTop: "var(--border-1)",
+            borderRight: "var(--border-1)",
+            borderBottom: "var(--border-1)",
+            borderLeft: "3px solid var(--blue)",
+            borderRadius: "3px",
+            padding: "1rem",
+            width: "40rem",
+            position: "relative",
+          }}
+        >
+          <div
+            style={{
+              fontWeight: "var(--w6)",
+              color: "#666",
+              left: "1rem",
+              ...anchoredStyle,
+            }}
+          >
+            Customer who satisfies{" "}
+            <span
+              style={{
+                backgroundColor: "var(--blue)",
+                color: "white",
+                padding: "0.05rem 0.4rem",
+                borderRadius: "999px",
+                fontSize: "0.78rem",
+                margin: "0 0.1rem",
+              }}
+            >
+              ALL
+            </span>{" "}
+            conditions
+          </div>
+          <div
+            style={{
+              fontWeight: "var(--w6)",
+              color: "var(--blue)",
+              right: "3rem",
+              ...anchoredStyle,
+            }}
+          >
+            Switch to advanced editor
+          </div>
+          <img
+            src={images.trash}
+            alt=""
+            style={{
+              right: "1rem",
+              ...anchoredStyle,
+              outline: '0.2rem solid white',
+              boxSizing: "content-box",
+              borderRadius: '999px',
+              padding: "0.25rem",
+              height: "0.85rem",
+              backgroundColor: "var(--bg-red)",
+              transform: 'translate(0, calc(-50% - 0.05rem))',
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }

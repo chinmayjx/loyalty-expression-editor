acfcdf748ec2c8f7b75c15ecc8834d41c48df815
chinmayji <chinmayjain08@gmail.com>
2022-06-21 15:59:25 +0530

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="main-flex">
+        <GroupBlock />
+        <ConditionsBlock />
+        <ConditionEditBlock />
+      </div>
     </div>
   );
 }
 
-export default App;
+function GroupBlock({ list = ["Set 1", "Set 2", "Set 3", "Set 4", "Set 5"] }) {
+  const [selectedLI, setSelectedLI] = useState(0);
+  return (
+    <div className="main-flex-item">
+      <div className="block-title-box">Groups</div>
+      <ul>
+        {list.map((x, i) => {
+          let style =
+            i === selectedLI ? { backgroundColor: "var(--bg-blue)", color: 'var(--blue)' } : {};
+          return (
+            <li
+              key={i}
+              style={{ padding: "0.4rem 1.25rem", fontWeight: 'var(--w6)', ...style }}
+              onClick={(e) => setSelectedLI(i)}
+            >
+              {x}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function ConditionsBlock() {
+  return (
+    <div className="main-flex-item">
+      <div className="block-title-box">All Conditions</div>
+    </div>
+  );
+}
+
+function ConditionEditBlock() {
+  return (
+    <div className="main-flex-item">
+      <div className="block-title-box">Condition 1</div>
+    </div>
+  );
+}
